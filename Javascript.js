1139897a473c1fb1e47e566490fa4f9fd046bc69
etliu <eric.t.liu15@gmail.com>
2016-10-22 15:43:17 -0700

    added earthquake data

--- Javascript.js
+++ Javascript.js
@@ -1,19 +1,41 @@
-$(document).ready(function(){
 
-});
-
-var piparray;
+var piparray = [];
+var dataarray = [];
+var map;
 
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044}
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
     zoom: 3,
-    center: {lat: 25.77317554565933, lng: -171.7407480830226}
+    center: {lat: 19.148917, lng: 4.265526}
   });
+  getData();
+  map.data.setStyle(function(feature) {
+  var magnitude = feature.getProperty('mag');
+  return {
+    icon: getCircle(magnitude)
+  };
+});
+}
+
+function getCircle(magnitude) {
+  return {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'red',
+    fillOpacity: .2,
+    scale: Math.pow(2, magnitude)/2,
+    strokeColor: 'white',
+    strokeWeight: .5
+  };
 }
 
 function getData () {
-  $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(){
+  $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+  function(data){
+      //$.each(data.features, function (index, value){
+        //dataarray.push(value);
+      //})
+        map.data.addGeoJson(data);
 
+      //console.log(dataarray)
   });
-}
+};
